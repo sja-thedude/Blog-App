13bcc470ea618756092ea1943799e31e4877b675
Syeda Juveria Afreen <sja.affu765@gmail.com>
2022-03-02 13:42:33 +0300

    Files updated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,17 +15,26 @@ class PostsController < ApplicationController
 
   def create
     new_post = current_user.posts.new(post_params)
-
-    if new_post.save
-      redirect_to user_posts_path(new_post.user.id, new_post.id)
-    else
-      render :new, alert: 'Error occured!'
+    new_post.likes_counter = 0
+    new_post.comments_counter = 0
+    new_post.update_posts_counter
+    respond_to do |format|
+      format.html do
+        if new_post.save
+          redirect_to "/users/#{new_post.user.id}/posts/", notice: 'Success!'
+        else
+          render :new, alert: 'Error occured!'
+        end
+      end
     end
   end
 
   def destroy
-    @post = Post.find(params[:id])
-    @post.destroy!
+    post = Post.find(params[:id])
+    user = User.find(post.user_id)
+    user.posts_counter -= 1
+    post.destroy
+    user.save
     flash[:success] = 'You have deleted this post!'
     redirect_to user_path(current_user.id)
   end
